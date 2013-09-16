c513b840b12b602c72a4d6aeb3e377fb3c7328ac
Adrian Bogatu <bogatu.adrian@gmail.com>
2013-09-16 15:35:23 +0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Courselink::Application.config.secret_key_base = '8128563556b2d6ccf678f158dd4eca43bdecd1d9b59c9c3ee343ffef25cdd8124d1cad7e9d1f6353eb2913064abb055d5fe36e46c00529f414bd55db30e37b41'
