72fb806e9a69c81b1c263fb1cf4d73bf25a3cc35
Adrian Bogatu <bogatu.adrian@gmail.com>
2013-09-16 16:14:38 +0300

    Made a welcoming homepage

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Courselink::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   match '/home', to: 'welcome#home', via: 'get'
+  match '/signup', to: 'welcome#home', via: 'get'
+  match '/signin', to: 'welcome#home', via: 'get'
 
   # You can have the root of your site routed with "root"
   root 'welcome#home'
