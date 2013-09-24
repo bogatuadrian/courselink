badfe4bc311a9db59554e3b3b5aa893ed022e93b
Adrian Bogatu <bogatu.adrian@gmail.com>
2013-09-24 11:45:03 +0300

    Added User model

---
+++ db/schema.rb
@@ -0,0 +1,60 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20130916164118) do
+
+  create_table "courses", force: true do |t|
+    t.string   "name"
+    t.string   "acronym"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "courses", ["name", "acronym"], name: "index_courses_on_name_and_acronym"
+
+  create_table "semesters", force: true do |t|
+    t.integer  "semester"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "semesters", ["semester"], name: "index_semesters_on_semester"
+
+  create_table "universities", force: true do |t|
+    t.string   "name"
+    t.string   "acronym"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "universities", ["name"], name: "index_universities_on_name"
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["name", "email"], name: "index_users_on_name_and_email"
+
+  create_table "years", force: true do |t|
+    t.integer  "year"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "years", ["year"], name: "index_years_on_year"
+
+end
