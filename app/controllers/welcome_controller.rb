bf4788b41c4145e70af9453a8524f2f96521c39e
Adrian Bogatu <bogatu.adrian@gmail.com>
2013-12-14 20:07:48 +0200

    Added dropdown for question's course selection

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,21 @@ class WelcomeController < ApplicationController
 
 	def show
 		@question = Question.new
+
+    universities = University.all
+
+    res = []
+
+    universities.each do |uni|
+      ary = []
+      uni.courses.each do |course|
+        ary << "#{uni.alias}/#{course.alias}"
+      end
+
+      res << ary
+    end
+
+    @array = res
 	end
 
 end
