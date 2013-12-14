72f2fd921870806b73a045a1f26ddede7b1b1b6f
Razvan Florea <razvan.florea91@gmail.com>
2013-12-14 16:55:20 +0200

    Migration added and tables modified

--- db/migrate/20130926063812_create_questions.rb
+++ db/migrate/20130926063812_create_questions.rb
@@ -3,7 +3,6 @@ class CreateQuestions < ActiveRecord::Migration
     create_table :questions do |t|
       t.string :title
       t.text :content
-      t.integer :user_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131214141253) do
+ActiveRecord::Schema.define(version: 20131214143655) do
 
   create_table "answers", force: true do |t|
     t.text     "content"
@@ -23,12 +23,12 @@ ActiveRecord::Schema.define(version: 20131214141253) do
 
   create_table "courses", force: true do |t|
     t.string   "name"
-    t.string   "acronym"
+    t.string   "alias"
+    t.integer  "year"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "university_id"
   end
-
-  add_index "courses", ["name", "acronym"], name: "index_courses_on_name_and_acronym"
 
   create_table "questions", force: true do |t|
     t.string   "title"
@@ -38,22 +38,12 @@ ActiveRecord::Schema.define(version: 20131214141253) do
     t.integer  "user_id"
   end
 
-  create_table "semesters", force: true do |t|
-    t.integer  "semester"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "semesters", ["semester"], name: "index_semesters_on_semester"
-
   create_table "universities", force: true do |t|
     t.string   "name"
-    t.string   "acronym"
+    t.string   "alias"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "universities", ["name"], name: "index_universities_on_name"
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -66,13 +56,5 @@ ActiveRecord::Schema.define(version: 20131214141253) do
 
   add_index "users", ["name", "email"], name: "index_users_on_name_and_email"
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
-
-  create_table "years", force: true do |t|
-    t.integer  "year"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "years", ["year"], name: "index_years_on_year"
 
 end
