5a1bfc2a7f45ec3d415d982008b8ad6842f5df75
Razvan Florea <razvan.florea91@gmail.com>
2013-12-15 12:25:45 +0200

    Stil debugging

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery-1.10.2.min
+//= require jquery
 //= require jquery_ujs
 //= require bootstrap
 //= require turbolinks
-//= require script
 //= require_tree .
