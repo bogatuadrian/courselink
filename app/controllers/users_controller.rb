1edcf8c9eba5efefcd7e57b52af2448fbcbff3c7
Razvan Florea <razvan.florea91@gmail.com>
2013-12-15 16:53:12 +0200

    Added user activity on user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @questions = @user.questions
+    @nr_q = @user.questions.length
+    @nr_a = @user.answers.length
   end
 
   def create
